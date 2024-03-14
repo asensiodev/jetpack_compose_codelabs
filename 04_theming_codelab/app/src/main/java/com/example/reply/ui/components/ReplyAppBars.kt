648188b6fdff23368b3a34e4e406792c4ddc91e0
asensiodev <asensiodev@gmail.com>
2024-03-14 22:11:39 +0100

    Use shapes

--- 04_theming_codelab/app/src/main/java/com/example/reply/ui/components/ReplyAppBars.kt
+++ 04_theming_codelab/app/src/main/java/com/example/reply/ui/components/ReplyAppBars.kt
@@ -22,7 +22,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
@@ -38,7 +38,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.reply.R
@@ -50,8 +49,10 @@ fun ReplySearchBar(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .clip(shape = RoundedCornerShape(15.dp))
-            .background(MaterialTheme.colorScheme.background),
+            .background(
+                MaterialTheme.colorScheme.background,
+                CircleShape
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
