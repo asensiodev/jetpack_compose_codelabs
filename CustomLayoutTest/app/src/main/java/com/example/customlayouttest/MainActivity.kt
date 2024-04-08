ae8ad97ae20436f98671a728b561cac9acd0ae01
asensiodev <asensiodev@gmail.com>
2024-04-08 17:05:06 +0200

    Add timeGraph

--- CustomLayoutTest/app/src/main/java/com/example/customlayouttest/MainActivity.kt
+++ CustomLayoutTest/app/src/main/java/com/example/customlayouttest/MainActivity.kt
@@ -1,39 +1,13 @@
 package com.example.customlayouttest
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.customlayouttest.ui.theme.CustomLayoutTestTheme
-import java.time.DayOfWeek
-import java.time.format.TextStyle
-import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,30 +24,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-
-
-
-@Composable
-fun TimeGraph(
-    hoursHeader: @Composable () -> Unit,
-    rowCount: Int,
-    dayLabel: @Composable (index: Int) -> Unit,
-    sleepBar: @Composable (index: Int) -> Unit,
-    modifier: Modifier = Modifier
-) {
-
-    val dayLabels = @Composable { repeat(rowCount) { dayLabel(it) } }
-    val sleepBars = @Composable { repeat(rowCount) { sleepBar(it) } }
-
-    /*Layout(
-        contents = listOf(hoursHeader, dayLabels, sleepBars),
-        modifier = Modifier
-    ) {
-
-    }
-*/
 }
 
 
