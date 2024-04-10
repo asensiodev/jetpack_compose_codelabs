7637120eaa994bfa7bf19ddce8e6913cf77d6273
asensiodev <asensiodev@gmail.com>
2024-04-10 17:22:06 +0200

    Show landing screen

--- 06_advanced_state_codelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ 06_advanced_state_codelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -21,6 +21,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -32,9 +34,13 @@ private const val SplashWaitTime: Long = 2000
 @Composable
 fun LandingScreen(onTimeout: () -> Unit, modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(newValue = onTimeout)
+
         LaunchedEffect(Unit) {
             delay(SplashWaitTime)
-            onTimeout()
+            currentOnTimeout()
         }
         // TODO: Make LandingScreen disappear after loading data
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
